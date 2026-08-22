ec38b45c6c761a68ad7e8fcd03744dad6ffe31e5
Tanbin Ahmed <95366983+tanbin02@users.noreply.github.com>
2026-08-22 16:43:06 +0600

    Update cart.js

--- cart.js
+++ cart.js
@@ -3,7 +3,7 @@ const CART_KEY = "footerpath_cart";
 const ORDERS_KEY = "footerpath_orders"; // backup only
 
 // =============== GOOGLE SHEETS ===============
-const SHEETS_API_URL = "https://script.google.com/macros/s/AKfycbzWKUYGw7XMaaP6o4wG0k4nxoJL1_F460nXr9HNAY4mU86RxPOxje9oDT7qns75aNn3/exec";
+const SHEETS_API_URL = "https://script.google.com/macros/s/AKfycbxZg_Fy6GoV0fdmHByORz_KOPpoxeJINuFL-HrKbR0EidcsB-7boNNSqCldT1gKxJ_O/exec";
 
 async function saveOrderToSheet(order) {
   try {
